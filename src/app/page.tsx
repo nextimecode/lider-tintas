71fe118dc7af7039f1116a2af23d63175c7c8810
Pedro Duarte <pedrohlduarte@gmail.com>
2023-10-08 06:07:48 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { pageQuery } from "@/lib/queries";
 import { parsePageData } from '@/utils/_parsePageData'
-import { Breakpoint, Grid, LogoCloud, PricingPlanSection, StatSection, Testimonial, Hero } from "@/components";
+import { Breakpoint, Grid, LogoCloud, PricingPlanSection, StatSection, Testimonial, Hero, Footer } from "@/components";
 
 async function getPost(slug: string) {
   const apolloUri = process.env.NEXT_PUBLIC_HYGRAPH_ENDPOINT as string
@@ -41,6 +41,7 @@ export default async function Home() {
       <StatSection columns={page.blocks[2].columns} gridSubtitle={page.blocks[2].gridSubtitle} gridTitle={page.blocks[2].gridTitle} />
       <Testimonial content={page.blocks[5].content} person={page.blocks[5].person} />
       <Hero buttons={page.hero.buttons} image={page.hero.image} navigation={page.navigation} page={page} />
+      <Footer primaryLinks={page.footer.primaryLinks} secondaryLinks={page.footer.secondaryLinks} />
     </>
   )
 }
--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -2,3 +2,4 @@ export * from './Navigation';
 export * from './Wrapper';
 export * from '../organisms/blocks';
 export * from './Hero';
+export * from './Footer';
